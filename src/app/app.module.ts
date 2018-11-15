013b20cbba3805923a31c38706e9524df80658b7
Friedrich Laudanum <niobiumz41@gmail.com>
2018-11-16 00:01:36 +0100

    Completed Part 3 Chapter 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,16 +10,23 @@ import { FormsModule } from '@angular/forms';
 
 // Importation of service AppareilService
 import { AppareilService } from './services/appareil.service';
-import { AuthService } from './services/auth.service'
+import { AuthService } from './services/auth.service';
+import { AuthGuard } from './services/auth-guard.service';
 import { AuthComponent } from './auth/auth.component';
 import { AppareilViewComponent } from './appareil-view/appareil-view.component';
 
 import { RouterModule, Routes } from '@angular/router';
+import { SingleAppareilComponent } from './single-appareil/single-appareil.component';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
 
 const appRoutes: Routes = [
-  {path: 'appareils', component: AppareilViewComponent},
+  {path: 'appareils', canActivate: [AuthGuard], component: AppareilViewComponent},
+  {path: 'appareils/:id', canActivate: [AuthGuard], component: SingleAppareilComponent},
   {path: 'auth', component: AuthComponent},
-  {path: '', component: AppareilViewComponent}
+  {path: '', component: AppareilViewComponent},
+  {path: 'not-found', component: FourOhFourComponent},
+  // The wildcard ** is the default URL and must be consequently introduced in last position
+  {path: '**', redirectTo: '/not-found'}
 ]
 
 @NgModule({
@@ -28,7 +35,9 @@ const appRoutes: Routes = [
     MonPremierComponent,
     AppareilComponent,
     AuthComponent,
-    AppareilViewComponent
+    AppareilViewComponent,
+    SingleAppareilComponent,
+    FourOhFourComponent
   ],
   imports: [
     BrowserModule,
@@ -38,7 +47,8 @@ const appRoutes: Routes = [
   // Angular doc: https://angular.io/guide/providers
   providers: [
     AppareilService,
-    AuthService
+    AuthService,
+    AuthGuard
   ],
   bootstrap: [AppComponent]
 })
